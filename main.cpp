c49d208a1585e17949304fc053b3f53da3a56a32
Kyubiso <jakka.kontakt@gmail.com>
2024-06-21 12:16:48 +0200

    Added Description in polish

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ Zadanie: Zaprojektować symulacje systemu kolejek w którym klienci poruszają s
 na jedną  trzech stacji przed Dystrybutor. Klienci poruszają się niezależnie z losowymi prędkościami i zatrzymują się
 na chwilę na jednej z trzech stacji końcowych. 
 Wymagania na Etap 2:
-    -Tylko jeden klient może być obsługiwany na raz w jednej stacji
+    -Tylko jeden klient może być obsługiwany na raz w jednej stacji, klienci muszą poczekać na zwolnienie stacji
     -Najbardziej zajęta stacja ma być wyłączona z puli stacji, do których idą klienci*/
 
 namespace std {
